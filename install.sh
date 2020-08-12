07c9738fae9e9bc739879402c4249a95a13d09b9
Erkan Yılmaz <33496216+erkanyilmazzz@users.noreply.github.com>
2020-08-12 13:48:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,37 @@
-  
+    
 #!/bin/sh
 #Erkan Yılmaz
 #date 11/02/2020
 
+
+#install all requarements
 install_req(){
 
     echo "instalation is starting"
-    sudo apt-get install -y docker.io                #need for jenkins
-    echo "docker instaled"
-    sudo apt-get install -y virtualbox               #need for vagrant
-    echo "virtual box instaled"
-    sudo apt-get install -y vagrant                  #need for freebsd machine
-    echo "vagrant  instaled"
-    sudo apt-get install -y git                      #need for vagrantfile from git_repo
-    echo "git instaled"
+    sudo apt-get install -y docker.io || 
+    { echo "error occurred while installing docker"; exit 1; }
+    && echo "docker instaled"
+    
+    sudo apt-get install -y virtualbox || 
+    { echo "error occurred while installing docker"; exit 1; }
+    && echo "docker instaled"
+    
+    sudo apt-get install -y vagrant  || 
+    { echo "error occurred while installing docker"; exit 1; }
+    && echo "docker instaled"
+    
+    sudo apt-get install -y git || 
+    { echo "error occurred while installing docker"; exit 1; }
+    && echo "docker instaled"
+    
 
 }
 
-run_jenkins(){
 
+#run a docker container(jenkins 2.60.3) in port 8080  
+run_jenkins(){
     read -p "please enter a jenkins_home path" JENKINS_HOME_PATH
     echo "path is $JENKINS_HOME_PATH"
-
     mkdir -p $JENKINS_HOME_PATH
     sudo docker pull  jenkins:2.60.3
     sudo docker run -p 8080:8080 -p 50000:50000 -v $JENKINS_HOME_PATH:/var/jenkins_home -d  jenkins:2.60.3
@@ -29,6 +39,7 @@ run_jenkins(){
 
 
 }
+
 run_vagrant(){
 
     read -p "please enter a path for vagrant freebsd" FREEBSD_PATH
@@ -37,19 +48,21 @@ run_vagrant(){
 
     mkdir -p $FREEBSD_PATH
     cd $FREEBSD_PATH
+
+    #git path for vagrant config 
+    #####asldında bunun yerine ssh kullanarak da servera indirebiliriz
     GIT_PATH="http://stash.logo.com.tr/projects/SGT/repos/build-staj/browse/vagrant_config/Vagrantfile?at=refs%2Fheads%2Fbranc_build_staj"
 
     if [0 !=$(git clone $GIT_PATH)]
-    then
-        echo "cant find vagrant file check your vpn or githup repo"
-        exit -1
-    else
-        echo "vagrant file cloned"
+        then
+            echo "cant find vagrant file check your vpn or githup repo"
+            exit -1
+        else
+            echo "vagrant file cloned"
     fi
 
-    vagrant init 
     vagrant up
-    
+
 }
 
 install_req
